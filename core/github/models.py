d3fa6452cfc59a5159e7f565aeda31643c7a2d21
Vitor <vitor.gripa98@gmail.com>
2022-01-16 23:37:47 +0000

    Adicionado models e migrations das primeiras tabelas

--- core/github/models.py
+++ core/github/models.py
@@ -1,3 +1,32 @@
-from django.db import models
+from django.db.models import Model
+from django.db.models import IntegerField
+from django.db.models import TextField
+from django.db.models import URLField
+from django.db.models import ForeignKey
+from django.db.models import CASCADE
 
-# Create your models here.
+from django.db.models import Manager
+
+
+class Linguagem(Model):
+    
+    nome = TextField()
+    objects = Manager()
+
+    class Meta:
+        db_table = "linguagens"
+        verbose_name = "Linguagem"
+        verbose_name_plural = "Linguagens"
+
+
+class Repositorio(Model):
+    nome = TextField()
+    url = URLField()
+    starts = IntegerField()
+    linguagem = ForeignKey(Linguagem, on_delete=CASCADE)
+    objects = Manager()
+
+    class Meta:
+        db_table = "repositorios"
+        verbose_name = "Repositório"
+        verbose_name_plural = "Repositórios"
